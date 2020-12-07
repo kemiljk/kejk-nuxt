91d8814f38618de9313358c87827e02096afb323
Karl Koch <hi@kejk.co>
2020-12-07 20:32:40 +0000

    fix: added publication date to post detail

--- components/Blog.vue
+++ components/Blog.vue
@@ -1,10 +1,13 @@
 <template>
     <div class="mt-0 pt-4">
-    <div class="flex flex-row text-left">
+    <div class="flex flex-row text-left"  v-if="blog.metadata">
       <div class="flex flex-col col-span-full md:col-span-6">
         <h1 class="my-4 text-4xl font-black dark:text-gray-200">
           {{ blog.title }}
         </h1>
+        <p class="text-gray-500 dark:text-gray-400 pt-0 pb-8">
+        {{ blog.metadata.published | moment("DD MMM, YYYY") }}
+        </p>
       </div>
     </div>
     <div class="flex flex-row px-lg-12">
