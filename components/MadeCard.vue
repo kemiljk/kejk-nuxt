b9c7cdfdf7b62c014c38aa425f49820aecc16998
Karl Koch <karl@kejk.tech>
2020-12-08 20:28:16 +0000

    fix: argh stupid styling
    I wish I could run this on localhost on my iPad

--- components/MadeCard.vue
+++ components/MadeCard.vue
@@ -2,9 +2,9 @@
   <div v-else class="flex flex-cols-6 sm:flex-cols-3 mb-0 p-4 bg-gray-100 dark:bg-gray-900 hover:shadow-lg hover:bg-gray-50 dark:hover:bg-gray-800 transition ease-in-out duration-300 rounded-xl">
       <a :href="made.href" target="_blank" rel="noreferrer">
           <img alt="Px to Em logo visual" :src="`${made.img}`" class="rounded-md" />
-          <button class="flex mt-2 text-md text-gray-900 dark:text-gray-50 font-semibold mx-auto py-2 rounded-md">
+          <button class="flex mt-2 text-md text-gray-900 dark:text-gray-50 font-semibold mx-auto py-2 rounded-md align-center">
               {{ made.title }}
-              <div v-if="made.id" class="ml-4 bg-yellow-400 -mb-2 px-3 py-1 rounded-full"><span :id="made.id" class="text-black mb-1 mr-2 align-center" /><ion-icon name="arrow-down-outline" class="text-black mt-1"></ion-icon></div>
+              <div v-if="made.id" class="ml-4 bg-yellow-400 -mb-2 px-3 py-1 rounded-full"><span :id="made.id" class="text-black mb-1 mr-2" /><ion-icon name="arrow-down-outline" class="text-black mt-1"></ion-icon></div>
           </button>
       </a>
   </div>  
