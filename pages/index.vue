78da3ecad43cc4ab5a996fdef2adb8a501c59037
Karl Koch <hi@kejk.co>
2020-12-09 20:14:07 +0000

    fix: nav broken when using container class instead of relative

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="container mx-auto">
+  <div class="relative mx-auto">
     <nav class="fixed flex bg-white dark:bg-black w-full pt-4 border-b-2 border-gray-100 dark:border-gray-900">
         <ul class="mx-auto">
-            <li class="flex space-x-4">
+            <li class="flex space-x-4 m-0">
                 <NuxtLink
                 isActive
                 to="/"
