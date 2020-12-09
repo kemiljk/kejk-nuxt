c8a6526494a31e16f4db674ba088d957459031c9
Karl Koch <karl@kejk.tech>
2020-12-09 07:30:04 +0000

    almost there

--- components/MadeCard.vue
+++ components/MadeCard.vue
@@ -2,8 +2,8 @@
   <div class="flex flex-cols-6 md:flex-cols-3 p-4 bg-gray-100 dark:bg-gray-900 hover:shadow-lg hover:bg-gray-50 dark:hover:bg-gray-800 transition ease-in-out duration-300 rounded-xl">
       <a :href="made.href" target="_blank" rel="noreferrer">
           <img alt="Px to Em logo visual" :src="`${made.img}`" class="rounded-md" />
-          <div class="flex flex-col align-center my-auto">
-          <button class="flex mt-2 text-md text-gray-900 dark:text-gray-50 font-semibold mx-auto py-2 rounded-md">
+          <div class="flex flex-col align-middle my-auto">
+          <button class="flex mt-2 text-md text-gray-900 dark:text-gray-50 font-semibold mx-auto py-2 rounded-md align-middle">
               {{ made.title }}
               <div v-if="made.id" class="ml-4 bg-yellow-400 -mb-3 px-3 py-1 rounded-full"><span :id="made.id" class="text-black pb-2 mr-2 align-middle" /><ion-icon name="code-download-outline" class="text-black align-middle"></ion-icon></div>
           </button>
