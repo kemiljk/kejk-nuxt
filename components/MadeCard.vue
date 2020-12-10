5c3cbeda98fdf7e930d4f4f6c78112268a44ba92
Karl Koch <hi@kejk.co>
2020-12-10 13:10:09 +0000

    fix: minor updates to plugin downloads asset

--- components/MadeCard.vue
+++ components/MadeCard.vue
@@ -5,7 +5,7 @@
           <div class="flex flex-col align-middle my-auto">
           <button class="flex mt-2 text-md text-gray-900 dark:text-gray-50 font-semibold mx-auto py-2 rounded-md align-middle">
               {{ made.title }}
-              <div v-if="made.id" class="ml-4 bg-yellow-400 -mb-3 px-3 py-1 rounded-full"><span :id="made.id" class="text-black pb-2 mr-2 align-middle" /><ion-icon name="code-download-outline" class="text-black align-middle"></ion-icon></div>
+              <div v-if="made.id" class="ml-4 bg-yellow-500 dark:text-yellow-300 -mb-3 -mt-1 px-4 py-1 rounded-full"><span :id="made.id" class="text-black align-middle" /></div>
           </button>
           </div>
       </a>
