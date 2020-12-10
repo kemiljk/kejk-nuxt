9fd8a1d198ab0b02d6364180561c325a7e7f713a
Karl Koch <hi@kejk.co>
2020-12-10 11:30:40 +0000

    fix: Dynamic post meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,64 +10,64 @@ export default {
     htmlAttrs: {
       lang: "en-GB",
     },
-    title: "KEJK – Product Designer and Developer",
-    meta: [
-      { charset: "utf-8" },
-      { name: "HandheldFriendly", content: "True" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      {
-        hid: "description",
-        name: "description",
-        content: "Human-first Product Designer and Developer.",
-      },
-      { property: "og:site_name", content: "KEJK" },
-      { hid: "og:type", property: "og:type", content: "website" },
-      {
-        hid: "og:url",
-        property: "og:url",
-        content: "https://www.kejk.tech",
-      },
-      {
-        hid: "og:title",
-        property: "og:title",
-        content: "Karl Emil James Koch",
-      },
-      {
-        hid: "og:description",
-        property: "og:description",
-        content: "Human-first Product Designer and Developer.",
-      },
-      {
-        hid: "og:image",
-        property: "og:image",
-        content: "https://res.cloudinary.com/kejk/image/upload/v1607350722/og-image_bcs2c8.png",
-      },
-      { property: "og:image:width", content: "740" },
-      { property: "og:image:height", content: "300" },
+    // title: "KEJK – Product Designer and Developer",
+    // meta: [
+    //   { charset: "utf-8" },
+    //   { name: "HandheldFriendly", content: "True" },
+    //   { name: "viewport", content: "width=device-width, initial-scale=1" },
+    //   {
+    //     hid: "description",
+    //     name: "description",
+    //     content: "Human-first Product Designer and Developer.",
+    //   },
+    //   { property: "og:site_name", content: "KEJK" },
+    //   { hid: "og:type", property: "og:type", content: "website" },
+    //   {
+    //     hid: "og:url",
+    //     property: "og:url",
+    //     content: "https://www.kejk.tech",
+    //   },
+    //   {
+    //     hid: "og:title",
+    //     property: "og:title",
+    //     content: "Karl Emil James Koch",
+    //   },
+    //   {
+    //     hid: "og:description",
+    //     property: "og:description",
+    //     content: "Human-first Product Designer and Developer.",
+    //   },
+    //   {
+    //     hid: "og:image",
+    //     property: "og:image",
+    //     content: "https://res.cloudinary.com/kejk/image/upload/v1607350722/og-image_bcs2c8.png",
+    //   },
+    //   { property: "og:image:width", content: "740" },
+    //   { property: "og:image:height", content: "300" },
 
-      { name: "twitter:site", content: "@bobross" },
-      { name: "twitter:card", content: "summary_large_image" },
-      {
-        hid: "twitter:url",
-        name: "twitter:url",
-        content: "https://www.kejk.tech",
-      },
-      {
-        hid: "twitter:title",
-        name: "twitter:title",
-        content: "Karl Emil James Koch",
-      },
-      {
-        hid: "twitter:description",
-        name: "twitter:description",
-        content: "Human-first Product Designer and Developer.",
-      },
-      {
-        hid: "twitter:image",
-        name: "twitter:image",
-        content: "https://res.cloudinary.com/kejk/image/upload/v1607350722/twitter-og_bbzz2a.png",
-      },
-    ],
+    //   { name: "twitter:site", content: "@bobross" },
+    //   { name: "twitter:card", content: "summary_large_image" },
+    //   {
+    //     hid: "twitter:url",
+    //     name: "twitter:url",
+    //     content: "https://www.kejk.tech",
+    //   },
+    //   {
+    //     hid: "twitter:title",
+    //     name: "twitter:title",
+    //     content: "Karl Emil James Koch",
+    //   },
+    //   {
+    //     hid: "twitter:description",
+    //     name: "twitter:description",
+    //     content: "Human-first Product Designer and Developer.",
+    //   },
+    //   {
+    //     hid: "twitter:image",
+    //     name: "twitter:image",
+    //     content: "https://res.cloudinary.com/kejk/image/upload/v1607350722/twitter-og_bbzz2a.png",
+    //   },
+    // ],
     link: [
       { rel: "icon", type: "image/svg+xml", href: "/favicon.ico" },
       {
