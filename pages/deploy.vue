a98f7463be2aed952dbc81ea1a0cadbf8e7ca757
Karl Koch <hi@kejk.co>
2020-12-14 15:02:07 +0000

    feat: adding in a new deploy button on hidden page

--- pages/deploy.vue
+++ pages/deploy.vue
@@ -2,26 +2,23 @@
   <div class="relative mx-auto">
     <Nav>
       <NavItem to="/">Home</NavItem>
-      <NavItem to="/about" isActive>About</NavItem>
+      <NavItem to="/about">About</NavItem>
       <NavItem to="/thoughts">Thoughts</NavItem>
       <NavItem to="/uses">Uses</NavItem>
     </Nav>
     <div class="max-w-3xl mx-auto px-4">
       <header class="container pt-10 pb-2 max-w-xl mx-auto">
         <h1 class="pt-16 pb-8 text-center">
-          /About
+          /Deploy
         </h1>
       </header>
-      <div class="flex flex-row">
-        <div class="flex flex-col">
-          <div class="my-8">
-            <a href="https://api.vercel.com/v1/integrations/deploy/prj_151ZIv2O8brPTtSO2oCMm7yx8Ekj/vyeyGIsHyW">
-              <button class="bg-indigo-100 dark:bg-indigo-600 px-4 py-2 rounded-md text-indigo-900 dark:text-indigo-100 font-medium">
-                Deploy site
-              </button>
-            </a>
-          </div>
-        </div>
+        <div class="flex flex-col md:flex-row justify-center mt-8">
+          <a href="https://api.vercel.com/v1/integrations/deploy/prj_151ZIv2O8brPTtSO2oCMm7yx8Ekj/vyeyGIsHyW">
+            <button class="flex flex-row justify-center text-center font-medium w-full bg-indigo-500 hover:bg-indigo-600 dark:bg-indigo-700 text-white py-4 px-6 rounded-xl transition ease-in-out duration-300">
+              Deploy site
+            </button>
+          </a>
+      </div>
       </div>
     </div>
   </div>
