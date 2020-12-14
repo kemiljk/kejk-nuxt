042ea985350716c5fc052baadbce9adea42187de
Karl Koch <hi@kejk.co>
2020-12-14 15:56:16 +0000

    feat: updated to utilise Inter as the default font if it can be loaded

--- nuxt.config.js
+++ nuxt.config.js
@@ -95,6 +95,10 @@ export default {
         rel: "canonical",
         href: "https://www.kejk.tech",
       },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap'
+      }
     ],
     script: [
       {
