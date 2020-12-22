5b5363f9850b8e395676f4c8ba6013a817a73980
Karl Koch <karl@kejk.tech>
2020-12-22 10:13:49 +0000

    feat: added updated date to Uses

--- pages/Uses.vue
+++ pages/Uses.vue
@@ -17,7 +17,7 @@
       </keep-alive>
       <p class="text-gray-500 dark:text-gray-400 px-4 pt-0 pt-2 font-medium">
         Updated {{ uses.modified_at | moment("from", "now") }}
-      </p>
+      </p> 
       <div
         class="mt-16 pb-16 border-t-2 border-gray-200 dark:border-gray-800"
       />
