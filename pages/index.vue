9e93e517848fecfca1fbc288f5a30133ddbaccf2
Karl <karl@kejk.tech>
2020-12-22 11:14:03 +0000

    fix: button

--- pages/index.vue
+++ pages/index.vue
@@ -94,7 +94,7 @@
           </div>
         </div>
       </div>
-      <NuxtLink to="/thoughts" class="flex flex-row justify-start w-max mt-2 text-black bg-transparent hover:bg-gray-1-0 dark:hover:bg-gray-800 dark:text-white dark:bg-transparent rounded-xl px-6 py-3">
+      <NuxtLink to="/thoughts" class="flex flex-row justify-start w-max mt-4 text-black bg-transparent hover:bg-gray-1-0 dark:hover:bg-gray-800 dark:text-white dark:bg-transparent rounded-xl px-4 py-2">
         See all
         <arrow-right-icon class="text-gray-700 dark:text-gray-300 ml-2"></arrow-right-icon>
       </NuxtLink>
