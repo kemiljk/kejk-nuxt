11d5d9c51aa2ff6cb72aa45f26f736c2063b7483
Karl <karl@kejk.tech>
2020-12-24 20:32:06 +0000

    fix: SocialCard

--- components/SocialCard.vue
+++ components/SocialCard.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="flex flex-cols-6 sm:flex-cols-3 mb-3 p-4 bg-gray-100 dark:bg-gray-900 hover:shadow-md rounded-xl hover:bg-gray-50 dark:hover:bg-gray-800 transition ease-in-out duration-300">
+  <div class="flex flex-cols-6 sm:flex-cols-3 p-4 bg-gray-100 dark:bg-gray-900 hover:shadow-md rounded-xl hover:bg-gray-50 dark:hover:bg-gray-800 transition ease-in-out duration-300">
       <a :href="social.href" target="_blank" rel="noreferrer" @click="window.fathom.trackGoal('WIWQ5IBA', 0);">
-          <twitter-icon v-if="social.twitter" size="1x" class="text-blue-400 mb-4"></twitter-icon>
-          <linkedin-icon v-if="social.linkedin" size="1x" class="text-blue-600 dark:text-blue-200 mb-4"></linkedin-icon>
-          <figma-icon v-if="social.figma" size="1x" class="text-red-900 dark:text-red-200 mb-4"></figma-icon>
-          <github-icon v-if="social.github" size="1x" class="dark:text-white text-black mb-4"></github-icon>
-          <dribbble-icon v-if="social.dribbble" size="1x" class="text-pink-400 mb-4"></dribbble-icon>
-          <music-icon v-if="social.music" size="1x" class="text-red-600 mb-4"></music-icon>
-          <div class="mt-2 text-md text-gray-900 dark:text-gray-50 font-bold mx-auto">
+          <twitter-icon v-if="social.twitter" size="1x" class="text-blue-400 pb-4"></twitter-icon>
+          <linkedin-icon v-if="social.linkedin" size="1x" class="text-blue-600 dark:text-blue-500 pb-4"></linkedin-icon>
+          <figma-icon v-if="social.figma" size="1x" class="text-green-600 dark:text-green-400 pb-4"></figma-icon>
+          <github-icon v-if="social.github" size="1x" class="dark:text-white text-black pb-4"></github-icon>
+          <dribbble-icon v-if="social.dribbble" size="1x" class="text-pink-400 pb-4"></dribbble-icon>
+          <music-icon v-if="social.music" size="1x" class="text-red-600 pb-4"></music-icon>
+          <div class="pt-2 text-md text-gray-900 dark:text-gray-50 font-bold">
               {{ social.title }}
           </div>
       </a>
