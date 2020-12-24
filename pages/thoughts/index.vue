44bee8450d431f34ae21b1f2a1fedbbee58f53d6
Karl <karl@kejk.tech>
2020-12-24 22:15:09 +0000

    fix: rss button

--- pages/thoughts/index.vue
+++ pages/thoughts/index.vue
@@ -17,10 +17,10 @@
           Thoughts about design, development and the intersection of the two.
         </p>
       </header>
-      <div class="flex flex-col md:flex-row justify-center mb-8">
+      <div class="flex flex-col justify-center mb-8 mx-auto">
         <a href="http://www.kejk.tech/feed.xml" @click="window.fathom.trackGoal('Q6WH9ACE', 0);">
           <button
-            class="flex flex-row justify-center text-center mx-2 font-medium w-full bg-indigo-100 dark:bg-indigo-600 px-6 py-3 rounded-md text-indigo-900 dark:text-indigo-100 hover:bg-indigo-200 dark:hover:bg-indigo-500"
+            class="flex flex-row justify-center text-center font-medium w-full bg-indigo-100 dark:bg-indigo-600 px-6 py-3 rounded-md text-indigo-900 dark:text-indigo-100 hover:bg-indigo-200 dark:hover:bg-indigo-500"
           >
           <rss-icon
                 size="1x"
