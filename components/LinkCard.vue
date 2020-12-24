1ebfe24d05b3ebd4e653e56bb20a6ec395da5ff4
Karl <karl@kejk.tech>
2020-12-24 18:58:39 +0000

    fix

--- components/LinkCard.vue
+++ components/LinkCard.vue
@@ -10,7 +10,7 @@
       class="hover:shadow-lg hover:bg-gray-50 dark:hover:bg-gray-800 transition ease-in-out duration-300 rounded-lg"
     >
       <header class="flex items-center justify-between px-4 pt-4 pb-2">
-        <h1 class="text-yellow-600 dark:text-yellow-300 text-lg font-bold">
+        <h1 class="text-yellow-600 dark:text-yellow-300 text-lg font-bold w-80">
           {{ link.title }}
         </h1>
         <external-link-icon
