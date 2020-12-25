dfb94751dcda9f7fecb95da8e8b86fb6325fef05
Karl <karl@kejk.tech>
2020-12-25 11:13:16 +0000

    fix: tag sizing

--- components/BlogCard.vue
+++ components/BlogCard.vue
@@ -15,7 +15,7 @@
       <p class="text-gray-500 dark:text-gray-400 px-4 pt-0 font-medium">
         Updated {{ blog.modified_at | moment("from", "now") }}
       </p>
-      <p class="ml-2 text-indigo-700 dark:text-indigo-400 border-2 border-indigo-700 dark:border-indigo-400 rounded-full px-3 py-1"
+      <p class="w-max text-indigo-700 dark:text-indigo-400 border-2 border-indigo-700 dark:border-indigo-400 rounded-full px-3 py-1"
         >
           {{ blog.metadata.tag }}
         </p>
