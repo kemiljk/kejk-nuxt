b37fb701cd1594629876773549e6fb040882af7c
Karl <karl@kejk.tech>
2020-12-25 14:57:30 +0000

    fix: tag bottom padding

--- components/BlogCard.vue
+++ components/BlogCard.vue
@@ -15,7 +15,7 @@
       <p class="text-gray-500 dark:text-gray-400 px-4 pt-0 font-medium">
         Updated {{ blog.modified_at | moment("from", "now") }}
       </p>
-      <div class="pl-4">
+      <div class="pl-4 pb-4">
       <!--<span class="w-max text-green-700 dark:text-green-400 border-2 border-green-700 dark:border-green-400 rounded-full px-3 py-1 text-xs mr-2" v-show="blog.modified_at.slice(0,1)">New</span>-->
       <span class="w-max text-indigo-700 dark:text-indigo-400 border-2 border-indigo-700 dark:border-indigo-400 rounded-full px-3 py-1 text-xs"
         >
