00e03d5115c28e185a19e1a42269fda4f979837b
Karl <karl@kejk.tech>
2020-12-30 20:21:10 +0000

    fix: alignment

--- components/BlogCard.vue
+++ components/BlogCard.vue
@@ -17,14 +17,14 @@
       <p class="text-gray-500 dark:text-gray-400 px-4 pt-0 font-medium">
         Updated {{ blog.modified_at | moment("from", "now") }}
       </p>
-      <div class="pl-4 pb-4">
+      <div class="flex pl-4 pb-4">
         <span
           class="flex w-max justify-center text-green-700 dark:text-green-400 border-2 border-green-700 dark:border-green-400 rounded-full px-3 py-1 text-xs mr-2"
           v-if="blog.metadata.latest === true"
           >New
         </span>
         <span
-          class="w-max text-center text-indigo-700 dark:text-indigo-400 border-2 border-indigo-700 dark:border-indigo-400 rounded-full px-3 py-1 text-xs"
+          class="flex w-max justify-center text-indigo-700 dark:text-indigo-400 border-2 border-indigo-700 dark:border-indigo-400 rounded-full px-3 py-1 text-xs"
         >
           {{ blog.metadata.tag }}
         </span>
