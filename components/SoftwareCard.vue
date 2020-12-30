76796904af073cdb2c310bf5042c822f533a42ca
Karl <karl@kejk.tech>
2020-12-30 20:24:20 +0000

    fix: flex again

--- components/SoftwareCard.vue
+++ components/SoftwareCard.vue
@@ -20,14 +20,14 @@
           ></external-link-icon>
         </header>
         <div class="divide-y divide-solid divide-gray-300 dark:divide-gray-800">
-          <div class="mb-4">
+          <div class="flex mb-4">
             <span
-              class="h-7 w-max text-center text-yellow-700 dark:text-yellow-400 border-2 border-yellow-700 dark:border-yellow-400 rounded-full px-3 py-1 text-xs mr-2"
+              class="flex w-max justify-center text-yellow-700 dark:text-yellow-400 border-2 border-yellow-700 dark:border-yellow-400 rounded-full px-3 py-1 text-xs mr-2"
               v-if="software.metadata.indie"
               >Indie</span
             >
             <span
-              class="h-7 w-max text-center text-purple-700 dark:text-purple-400 border-2 border-purple-700 dark:border-purple-400 rounded-full px-3 py-1 text-xs mr-2"
+              class="flex w-max justify-center text-purple-700 dark:text-purple-400 border-2 border-purple-700 dark:border-purple-400 rounded-full px-3 py-1 text-xs mr-2"
               v-if="software.metadata.open_source"
               >Open source</span
             >
