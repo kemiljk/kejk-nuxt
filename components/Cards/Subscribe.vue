de2e4b55910de4f5084bb35a5ff47bb030685975
Karl Koch <karl@kejk.tech>
2021-01-02 15:13:13 +0000

    fix: my-md input

--- components/Cards/Subscribe.vue
+++ components/Cards/Subscribe.vue
@@ -49,7 +49,7 @@
               <div class="flex flex-col w-full md:w-max mt-4">
                 <label for="bd-email" class="font-medium text-black dark:text-white">Enter your email</label>
                 <div class="flex flex-col md:flex-row md:space-x-2 w-full md:w-max">
-                  <input type="email" name="email" id="bd-email" placeholder="e.g. sly@stallone.com" class="mt-2 py-3 px-3 w-full md:w-max rounded-md bg-white dark:bg-black border-2 border-gray-200 dark:border-gray-700 text-gray-800 dark:text-gray-50 font-medium focus:border-indigo-500 focus:border-4 focus:outline-none"
+                  <input type="email" name="email" id="bd-email" placeholder="e.g. sly@stallone.com" class="my-2 py-3 px-3 w-full md:w-max rounded-md bg-white dark:bg-black border-2 border-gray-200 dark:border-gray-700 text-gray-800 dark:text-gray-50 font-medium focus:border-indigo-500 focus:border-4 focus:outline-none"
                     required />
                   <input type="hidden" value="1" name="embed" />
                   <button type="submit" class="flex flex-row justify-center text-center font-medium my-2 w-full md:w-max bg-indigo-600 px-6 py-3 rounded-md text-indigo-50 hover:bg-indigo-600">
