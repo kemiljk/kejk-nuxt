e8756cdbef3d714923bac720fc0eacee4bc8ad64
Karl <karl@kejk.tech>
2021-01-02 14:07:23 +0000

    fix: hover for padding

--- components/Cards/BlogCard.vue
+++ components/Cards/BlogCard.vue
@@ -4,7 +4,7 @@
       class="rounded-lg hover:shadow-lg hover:bg-gray-50 dark:hover:bg-gray-900 transition ease-in-out duration-300"
       v-if="blog.metadata"
     >
-      <header class="flex items-center justify-between px-4 pt-4 pb-2">
+      <header class="flex items-center justify-between hover:px-4 pt-4 pb-2">
         <h1
           class="text-indigo-600 dark:text-indigo-300 text-lg font-bold w-60 md:w-80"
         >
@@ -14,10 +14,10 @@
           class="text-gray-700 dark:text-gray-300"
         ></arrow-right-icon>
       </header>
-      <p class="font-mono text-gray-500 dark:text-gray-400 px-4 pt-0 font-medium">
+      <p class="font-mono text-gray-500 dark:text-gray-400 hover:px-4 pt-0 font-medium">
         Updated {{ blog.modified_at | moment("from", "now") }}
       </p>
-      <div class="flex space-x-2 pl-4 pb-4">
+      <div class="flex space-x-2 hover:pl-4 pb-4">
         <span
           class="flex w-max justify-center font-mono text-green-700 dark:text-green-400 border-2 border-green-700 dark:border-green-400 rounded-full px-3 py-1 text-xs"
           v-if="blog.metadata.latest === true"
@@ -30,7 +30,7 @@
         </span>
       </div>
       <p
-        class="text-gray-700 dark:text-gray-300 px-4 pt-2 pb-4 font-medium mb-0"
+        class="text-gray-700 dark:text-gray-300 hover:px-4 pt-2 pb-4 font-medium mb-0"
       >
         {{ blog.metadata.snippet }}
       </p>
