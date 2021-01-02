8a10e9255ac63655110c3b6c7a6145cb30486fe5
Karl Koch <karl@kejk.tech>
2021-01-02 14:50:02 +0000

    fix: button

--- components/Navigation/MoreLink.vue
+++ components/Navigation/MoreLink.vue
@@ -1,7 +1,7 @@
 <template>
     <NuxtLink
         to="/thoughts"
-        class="flex flex-row justify-start w-max mt-4 text-black bg-transparent hover:bg-gray-100 dark:hover:bg-gray-800 dark:text-white dark:bg-transparent rounded-xl px-4 py-2"
+        class="flex flex-row justify-start w-max mt-4 text-black bg-gray-50 dark:bg-gray-900 dark:text-white rounded-xl px-4 py-2"
       >
         <slot></slot>
         <arrow-right-icon
