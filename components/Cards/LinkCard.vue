a9851716f12f76d84ef85214e39cd32c77cdeb68
Karl Koch <karl@kejk.tech>
2021-01-02 14:48:24 +0000

    fix: hover

--- components/Cards/LinkCard.vue
+++ components/Cards/LinkCard.vue
@@ -3,12 +3,12 @@
     :href="link.metadata.url"
     target="`_blank"
     rel="noreferrer"
-    class="cursor-pointer mb-0"
+    class="cursor-pointer mb-0 hover:px-4 focused:px-4"
   >
     <div
       class="rounded-lg hover:shadow-lg hover:bg-gray-50 dark:hover:bg-gray-900 transition ease-in-out duration-300"
     >
-      <header class="flex items-center justify-between hover:px-4 focused:px-4 pt-4 pb-2">
+      <header class="flex items-center justify-between pt-4 pb-2">
         <h1
           class="text-yellow-600 dark:text-yellow-300 text-lg font-bold w-60 md:w-80"
         >
@@ -18,11 +18,11 @@
           class="text-gray-700 dark:text-gray-300"
         ></external-link-icon>
       </header>
-      <p class="font-mono text-gray-500 dark:text-gray-400 hover:px-4 focused:px-4 pt-0 pb-2 font-medium">
+      <p class="font-mono text-gray-500 dark:text-gray-400 pt-0 pb-2 font-medium">
         {{ link.metadata.published | moment("from", "now") }}
       </p>
       <p
-        class="text-gray-700 dark:text-gray-300 hover:px-4 focused:px-4 pb-4 font-medium mb-0"
+        class="text-gray-700 dark:text-gray-300 pb-4 font-medium mb-0"
         v-html="link.metadata.snippet"
       >
         {{ link.metadata.snippet }}
