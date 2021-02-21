e77fe23257d343361dbbd007c324fff3d4f06d70
Karl <karl@kejk.tech>
2021-02-21 22:17:17 +0000

    fix: button flex

--- components/Cards/SocialCard.vue
+++ components/Cards/SocialCard.vue
@@ -1,7 +1,7 @@
 <template>
     <a :href="social.href" target="_blank" rel="noreferrer">
   <button
-    class="flex flex-cols-6 sm:flex-cols-3 p-4 bg-gray-100 dark:bg-gray-900 hover:shadow-md transform hover:-rotate-2 rounded-xl hover:bg-gray-50 dark:hover:bg-gray-800 transition ease-in-out duration-300 w-full focus:outline-none focus:ring-2 focus:ring-white"
+    class="flex items-center justify-center p-4 bg-gray-100 dark:bg-gray-900 hover:shadow-md transform hover:-rotate-2 rounded-xl hover:bg-gray-50 dark:hover:bg-gray-800 transition ease-in-out duration-300 w-full focus:outline-none focus:ring-2 focus:ring-white"
   >
       <twitter-icon
         v-if="social.twitter"
@@ -33,7 +33,7 @@
         size="2x"
         class="text-red-600 pb-4"
       ></coffee-icon>
-      <div class="block text-md text-gray-900 dark:text-gray-50 font-bold">
+      <div class="flex text-md text-gray-900 dark:text-gray-50 font-bold">
         {{ social.title }}
       </div>
   </button>
