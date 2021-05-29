c322dcb250d8a8b521eff49a00efc4074c873ee6
Karl Koch <karl@kejk.tech>
2021-05-29 15:23:20 +0100

    fix: loading state for portfolio

--- pages/portfolio/_id.vue
+++ pages/portfolio/_id.vue
@@ -26,11 +26,10 @@
             class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
             style="z-index: -1;"
             xmlns="http://www.w3.org/2000/svg"
-            fill="none"
             viewBox="0 0 24 24"
           >
             <path
-              class="bg-gray-600 dark:bg-gray-400"
+              class="fill-current bg-gray-600 dark:bg-gray-400"
               d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
             ></path>
           </svg>
