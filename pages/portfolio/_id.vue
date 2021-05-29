c43a4478977c23e71ef7ae7bae7664c15dfc3eab
Karl Koch <karl@kejk.tech>
2021-05-29 14:36:36 +0100

    fix: loading state for portfolio

--- pages/portfolio/_id.vue
+++ pages/portfolio/_id.vue
@@ -22,7 +22,22 @@
           <BackLink link="portfolio"> Back to all</BackLink>
         </div>
         <div v-if="this.loading === true">
-          Loading...
+          <svg
+            class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+            style="z-index: -1;"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              class="opacity-75"
+              fill="black"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
+          <h1 class="text-md text-gray-600 dark:text-gray-400">
+            Loading...
+          </h1>
         </div>
         <Portfolio :portfolio="portfolio" />
         <div class="flex flex-row pt-16">
