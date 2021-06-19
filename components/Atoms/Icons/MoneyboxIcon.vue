15d9d7bb2dfc21075cd6a07c5a0e44774486b244
Karl <karl@kejk.tech>
2021-06-19 09:23:50 +0100

    fix: clipping icon edges

--- components/Atoms/Icons/MoneyboxIcon.vue
+++ components/Atoms/Icons/MoneyboxIcon.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="w-12 h-12 rounded-full">
     <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
     <rect width="48" height="48" rx="24" fill="white"/>
     <circle cx="23.9736" cy="24.1445" r="10.6498" fill="#00C2B5"/>
