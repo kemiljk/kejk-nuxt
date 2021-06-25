3d80fe335f5eba6571db5c14ec6afee8575eafd8
Karl <karl@kejk.tech>
2021-06-25 01:27:45 +0100

    fix: active tab bg

--- pages/thoughts/index.vue
+++ pages/thoughts/index.vue
@@ -33,7 +33,7 @@
           v-for="tab in tabs"
           :key="tab"
           @click="(selected = tab), (active = true)"
-          class="block font-medium rounded-md bg-gray-100 dark:bg-gray-800 px-4 py-2 text-gray-600 hover:text-gray-800 active:text-gray-800 dark:text-gray-400 dark:hover:text-gray-50 dark:active:text-gray-50 hover:bg-gray-200 dark:hover:bg-gray-800 active:bg-gray-200 dark:active:bg-gray-800"
+          class="block font-medium rounded-md bg-gray-100 dark:bg-gray-800 px-4 py-2 text-gray-600 hover:text-gray-800 active:text-gray-800 dark:text-gray-400 dark:hover:text-gray-50 dark:active:text-gray-50 hover:bg-gray-200 dark:hover:bg-gray-700 active:bg-gray-200 dark:active:bg-gray-700"
           :class="[
             {
               'text-gray-800 dark:text-gray-50 bg-gray-200 dark:bg-gray-700': selected === tab, active: selected === tab,
