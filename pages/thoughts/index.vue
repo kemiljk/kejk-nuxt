b19ee0fe916a5f8e52c2507d915554463adaae41
Karl <karl@kejk.tech>
2021-06-25 09:01:28 +0100

    fix: selected

--- pages/thoughts/index.vue
+++ pages/thoughts/index.vue
@@ -36,7 +36,7 @@
           class="block font-medium rounded-md bg-gray-100 dark:bg-gray-800 px-4 py-2 text-gray-600 hover:text-gray-800 active:text-gray-800 dark:text-gray-400 dark:hover:text-gray-50 dark:active:text-gray-50 hover:bg-gray-200 dark:hover:bg-gray-700 active:bg-gray-200 dark:active:bg-gray-700"
           :class="[
             {
-              'text-gray-800 dark:text-gray-50 bg-gray-200 dark:bg-gray-700': selected === tab, active: selected === tab,
+              selected: 'text-gray-800 dark:text-gray-50 bg-gray-200 dark:bg-gray-700', active: selected === tab,
             },
           ]"
         >
