7cc5e52a685426135bf6f527ca1eff0d712aa99f
Karl <karl@kejk.tech>
2021-08-16 07:56:07 +0100

    fix: removed double socials

--- pages/About.vue
+++ pages/About.vue
@@ -54,16 +54,6 @@
           </Button>
         </NuxtLink>
       </div>
-      <H2Header class="pt-16">Where to find me.</H2Header>
-      <div class="flex flex-row">
-        <div class="grid grid-row grid-cols-2 md:grid-cols-6 w-full gap-4">
-          <div v-for="social in socials" :key="social._id">
-            <keep-alive>
-              <SocialCard :social="social" />
-            </keep-alive>
-          </div>
-        </div>
-      </div>
       <Divider />
       <H2Header id="GET_IN_TOUCH">How to reach me.</H2Header>
       <GetInTouch />
@@ -112,38 +102,6 @@ export default {
       loading: false,
       tech: {},
       about: {},
-      socials: [
-        {
-          href: "https://www.figma.com/@_kejk",
-          figma: true,
-          title: "Figma",
-        },
-        {
-          href: "https://www.twitter.com/_kejk",
-          twitter: true,
-          title: "Twitter",
-        },
-        {
-          href: "https://www.Linkedin.com/in/kejk",
-          linkedin: true,
-          title: "LinkedIn",
-        },
-        {
-          href: "https://www.github.com/kemiljk/",
-          github: true,
-          title: "Github",
-        },
-        {
-          href: "https://dribbble.com/_kejk",
-          dribbble: true,
-          title: "Dribbble",
-        },
-        {
-          href: "https://www.ko-fi.com/kemiljk",
-          coffee: true,
-          title: "Ko-Fi",
-        },
-      ],
     };
   },
   created() {
