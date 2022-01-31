01e6bd1f1048c7c63372b3dbf5c2fb738eef8e1a
Karl <karl@kejk.tech>
2022-01-31 22:20:30 +0000

    fix: larger H1

--- components/Atoms/Header.vue
+++ components/Atoms/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="container pt-10 pb-2 max-w-xl mx-auto">
-    <h1 class="pt-16 pb-4 text-center text-4xl">
+    <h1 class="pt-16 pb-4 text-center text-6xl">
       <slot />
     </h1>
   </header>
