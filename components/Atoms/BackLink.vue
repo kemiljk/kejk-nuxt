f42bf5d5fb21f645f2375b95c1f3a6eaa0d0c634
Karl <karl@kejk.tech>
2022-02-01 08:27:05 +0000

    button z

--- components/Atoms/BackLink.vue
+++ components/Atoms/BackLink.vue
@@ -1,7 +1,7 @@
 <template>
   <NuxtLink
     :to="backLink"
-    class="text-black hover:bg-neutral-50 dark:hover:bg-neutral-900 dark:text-white border-2 border-neutral-500 rounded-xl px-4 py-2 no-underline transition ease-in-out duration-200"
+    class="z-10 text-black hover:bg-neutral-50 dark:hover:bg-neutral-900 dark:text-white border-2 border-neutral-500 rounded-xl px-4 py-2 no-underline transition ease-in-out duration-200"
   >
     <div class="flex items-center justify-between font-medium">
       <arrow-left-icon
