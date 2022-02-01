3a17f64fc7ca10c95c48ec933c47db768284bd9f
Karl <karl@kejk.tech>
2022-02-01 22:37:37 +0000

    text colour

--- components/Atoms/Subheader.vue
+++ components/Atoms/Subheader.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="pb-4 max-w-lg mx-auto">
-    <span class="text-2xl leading-snug text-left lg:text-center">
+    <span class="text-2xl leading-snug text-left lg:text-center text-black dark:text-white">
       <slot name="text" />
     </span>
     <div class="flex flex-col md:flex-row justify-center mt-6">
