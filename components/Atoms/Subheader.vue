de039c6f304df1cc75e3a79374666c7204c12db7
Karl <karl@kejk.tech>
2022-02-01 20:11:16 +0000

    fix: subtext

--- components/Atoms/Subheader.vue
+++ components/Atoms/Subheader.vue
@@ -1,8 +1,8 @@
 <template>
   <header class="pb-4 max-w-lg mx-auto">
-    <p class="text-2xl leading-snug text-left lg:text-center">
+    <span class="text-2xl leading-snug text-left lg:text-center">
       <slot name="text" />
-    </p>
+    </span>
     <div class="flex flex-col md:flex-row justify-center mt-6">
       <slot name="buttons" />
     </div>
