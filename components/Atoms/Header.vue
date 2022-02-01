bad78121cc3e11f11f45bd7a317b209b1aea90e1
Karl <karl@kejk.tech>
2022-02-01 08:01:55 +0000

    fix: larger text on article title

--- components/Atoms/Header.vue
+++ components/Atoms/Header.vue
@@ -1,6 +1,6 @@
 <template>
-  <header class="container pt-10 pb-2 max-w-xl mx-auto">
-    <h1 class="pt-16 pb-4 text-left lg:text-center text-6xl">
+  <header class="container pb-2 max-w-xl mx-auto">
+    <h1 class="pt-24 pb-4 text-left lg:text-center text-6xl">
       <slot />
     </h1>
   </header>
