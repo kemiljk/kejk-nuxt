153d730c70f9705ccbdbe8e0a433c5f2bfd1ebf5
Karl <karl@kejk.tech>
2022-02-01 07:11:34 +0000

    fix: larger H1

--- components/Atoms/Subheader.vue
+++ components/Atoms/Subheader.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="pb-4 max-w-lg mx-auto">
-    <p class="text-2xl text-center">
+    <p class="text-2xl text-left lg:text-center">
       <slot name="text" />
     </p>
     <div class="flex flex-col md:flex-row justify-center mt-6">
