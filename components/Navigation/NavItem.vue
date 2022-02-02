31613e25f01e18c909b34546e4558c0ae60f4040
Karl <karl@kejk.tech>
2022-02-02 19:28:44 +0000

    add flex

--- components/Navigation/NavItem.vue
+++ components/Navigation/NavItem.vue
@@ -7,7 +7,7 @@
         :class="[
           'w-full md:w-max flex items-center justify-center font-normal md:block px-4 py-2 text-neutral-900 dark:text-white md:border-b-2 md:border-transparent md:hover:border-b-2 md:hover:border-neutral-300 md:dark:hover:border-neutral-300 hover:bg-neutral-400 hover:text-black dark:hover:text-white dark:hover:bg-neutral-500 md:bg-transparent md:hover:bg-transparent rounded-md md:rounded-none transition ease-in-out duration-200 no-underline',
           {
-            'w-full md:w-max md:border-b-2 md:border-black md:dark:border-white bg-neutral-300 dark:bg-neutral-600 hover:bg-neutral-400 dark:hover:bg-neutral-500 text-neutral-900 dark:text-white hover:text-black dark:hover:text-white md:hover:border-black md:dark:hover:border-white md:bg-transparent md:hover:bg-transparent rounded-md md:rounded-none transition ease-in-out duration-200 no-underline': isActive,
+            'w-full md:w-max flex md:border-b-2 md:border-black md:dark:border-white bg-neutral-300 dark:bg-neutral-600 hover:bg-neutral-400 dark:hover:bg-neutral-500 text-neutral-900 dark:text-white hover:text-black dark:hover:text-white md:hover:border-black md:dark:hover:border-white md:bg-transparent md:hover:bg-transparent rounded-md md:rounded-none transition ease-in-out duration-200 no-underline': isActive,
           },
         ]"
       >
@@ -20,7 +20,7 @@
         :class="[
           'w-full md:w-max flex items-center justify-center font-normal md:block px-4 py-2 text-neutral-900 dark:text-white md:border-b-2 md:border-transparent md:hover:border-b-2 md:hover:border-neutral-300 md:dark:hover:border-neutral-300 hover:bg-neutral-400 dark:hover:bg-neutral-500 hover:text-black dark:hover:text-white md:bg-transparent md:hover:bg-transparent rounded-md md:rounded-none transition ease-in-out duration-200 no-underline',
           {
-            'w-full md:w-max md:border-b-2 md:border-black md:dark:border-white bg-neutral-300 dark:bg-neutral-600 hover:bg-neutral-400 dark:hover:bg-neutral-500 text-neutral-900 dark:text-white hover:text-black dark:hover:text-white md:hover:border-black md:dark:hover:border-white md:bg-transparent md:hover:bg-transparent rounded-md md:rounded-none transition ease-in-out duration-200 no-underline': isActive,
+            'w-full md:w-max flex md:border-b-2 md:border-black md:dark:border-white bg-neutral-300 dark:bg-neutral-600 hover:bg-neutral-400 dark:hover:bg-neutral-500 text-neutral-900 dark:text-white hover:text-black dark:hover:text-white md:hover:border-black md:dark:hover:border-white md:bg-transparent md:hover:bg-transparent rounded-md md:rounded-none transition ease-in-out duration-200 no-underline': isActive,
           },
         ]"
       >
