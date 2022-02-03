f919f671f012aca5738c6e706414f98c3e2beb82
Karl Koch <karl@kejk.tech>
2022-02-03 17:14:11 +0000

    fix: nav

--- components/Navigation/Nav.vue
+++ components/Navigation/Nav.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <nav>
-      <div class="bg-white dark:bg-black">
+      <div class="w-full h-16 bg-white dark:bg-black">
         <div
           class="w-full fixed z-50 backdrop-blur-2xl mx-auto pl-4 sm:pl-6 lg:px-8"
         >
--- dist/_nuxt/static/1643897329/Lazy-pdf/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/Lazy-pdf", {data:[{}],fetch:[],mutations:[]});
