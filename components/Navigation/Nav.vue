c495d798274a21163d00ab6f6eec35498bdebf78
Karl Koch <karl@kejk.tech>
2022-02-03 17:11:07 +0000

    fix: tailwind 3 naming

--- components/Navigation/Nav.vue
+++ components/Navigation/Nav.vue
@@ -3,7 +3,7 @@
     <nav>
       <div class="bg-white dark:bg-black">
         <div
-          class="w-full fixed z-50 backdrop-filter backdrop-blur-2xl mx-auto pl-4 sm:pl-6 lg:px-8"
+          class="w-full fixed z-50 backdrop-blur-2xl mx-auto pl-4 sm:pl-6 lg:px-8"
         >
           <div
             class="flex items-center justify-between w-full h-16 overflow-x-auto md:overflow-none scrollbar-hidden"
