1327cba9eaa45e1e6940439a45ad1ffa57d20ede
Karl <karl@kejk.tech>
2022-02-03 20:10:49 +0000

    footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,7 +5,7 @@
     </div>
     <div class="w-full flex justify-center items-center pt-16 px-4">
       <p
-        class="w-full md:w-2/3 lg:w-1/3 text-center p-4 bg-neutral-50 dark:bg-neutral-900 rounded-xl text-black dark:text-white"
+        class="w-full md:w-2/3 lg:w-1/3 text-center p-4 bg-neutral-50 dark:bg-neutral-900 rounded-3xl text-black dark:text-white"
       >
         This site, and all of my work, is dedicated to my hero, my inspiration,
         and the strongest man I've ever known. My father. I love you Dad ❤️.
