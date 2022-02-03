bd3199eefbec6b58b44582484db08ba2f36f452c
Karl Koch <karl@kejk.tech>
2022-02-03 17:19:56 +0000

    fix: nav

--- components/Navigation/Nav.vue
+++ components/Navigation/Nav.vue
@@ -3,7 +3,7 @@
     <nav>
       <div class="fixed w-full h-16 bg-white/90 dark:bg-black/90">
         <div
-          class="w-full fixed z-50 backdrop-blur-2xl mx-auto pl-4 sm:pl-6 lg:px-8"
+          class="w-full fixed z-50 backdrop-filter backdrop-blur-2xl mx-auto pl-4 sm:pl-6 lg:px-8"
         >
           <div
             class="flex items-center justify-between w-full h-16 overflow-x-auto md:overflow-none scrollbar-hidden"
