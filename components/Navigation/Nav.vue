16fe0ed2b7fec42e378b906d51978a846f01c333
Karl Koch <karl@kejk.tech>
2022-02-03 17:07:39 +0000

    fix: background of nav

--- components/Navigation/Nav.vue
+++ components/Navigation/Nav.vue
@@ -1,20 +1,22 @@
 <template>
-  <div class="bg-white dark:bg-black">
+  <div>
     <nav>
-      <div
-        class="w-full fixed z-50 backdrop-filter backdrop-blur-2xl mx-auto pl-4 sm:pl-6 lg:px-8"
-      >
+      <div class="bg-white dark:bg-black">
         <div
-          class="flex items-center justify-between w-full h-16 overflow-x-auto md:overflow-none scrollbar-hidden"
+          class="w-full fixed z-50 backdrop-filter backdrop-blur-2xl mx-auto pl-4 sm:pl-6 lg:px-8"
         >
-          <Logo class="mr-4 md:mr-0" />
-          <div class="flex items-center">
-            <div class="shrink-0">
-              <div class="inline-flex">
-                <div
-                  class="flex items-center justify-between space-x-1 md:space-x-4"
-                >
-                  <slot></slot>
+          <div
+            class="flex items-center justify-between w-full h-16 overflow-x-auto md:overflow-none scrollbar-hidden"
+          >
+            <Logo class="mr-4 md:mr-0" />
+            <div class="flex items-center">
+              <div class="shrink-0">
+                <div class="inline-flex">
+                  <div
+                    class="flex items-center justify-between space-x-1 md:space-x-4"
+                  >
+                    <slot></slot>
+                  </div>
                 </div>
               </div>
             </div>
