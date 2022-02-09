f67490a307e7f36fa763735514748f37daea3c8b
Karl <karl@kejk.tech>
2022-02-09 08:49:31 +0000

    text center

--- components/Atoms/Subheader.vue
+++ components/Atoms/Subheader.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="pb-4 max-w-xl mx-auto">
-    <span class="text-2xl leading-snug text-left lg:text-center text-black dark:text-white">
+    <span class="text-2xl leading-snug text-left md:text-center text-black dark:text-white">
       <slot name="text" />
     </span>
     <div class="flex flex-col md:flex-row justify-center mt-6">
