9fedfa93b0b346151741cebd45abe154d6a0f32c
Karl <karl@kejk.tech>
2022-02-10 07:55:28 +0000

    nav blur

--- components/Navigation/Nav.vue
+++ components/Navigation/Nav.vue
@@ -1,26 +1,12 @@
 <template>
-  <div>
-    <nav class="backdrop-blur-2xl bg-white dark:bg-black">
-      <div
-        class="w-full fixed z-50 mx-auto pl-4 sm:pl-6 lg:px-8"
-      >
+    <nav class="backdrop-blur-2xl bg-white dark:bg-black w-full fixed z-50 mx-auto pl-4 sm:pl-6 lg:px-8">
         <div
           class="flex items-center justify-between w-full h-16 overflow-x-auto md:overflow-none scrollbar-hidden"
         >
-          <Logo class="mr-6 md:mr-0" />
-          <div class="flex items-center">
-            <div class="shrink-0">
-              <div class="inline-flex">
-                <div
-                  class="flex items-center justify-between space-x-2 md:space-x-4 mr-6 md:mr-0"
-                >
-                  <slot></slot>
-                </div>
-              </div>
-            </div>
-          </div>
+          <Logo class="mr-6 md:mr-0 shrink-0" />
+            <div class="flex items-center justify-between shrink-0 space-x-2 md:space-x-4 mr-6 md:mr-0">
+          <slot></slot>
         </div>
       </div>
     </nav>
-  </div>
 </template>
