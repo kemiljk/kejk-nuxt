ff32b34c68f2e632008f8dbd9935fe7a2c020e23
Karl Koch <karl@kejk.tech>
2022-02-10 09:00:48 +0000

    fix: nav blur

--- components/Navigation/Nav.vue
+++ components/Navigation/Nav.vue
@@ -1,12 +1,18 @@
 <template>
-    <nav class="bg-white dark:bg-black w-full fixed z-50 mx-auto pl-4 sm:pl-6 lg:px-8">
+  <nav class="bg-white dark:bg-black">
+    <div
+      class="fixed z-50 mx-auto w-full pl-4 backdrop-blur-2xl backdrop-filter sm:pl-6 lg:px-8"
+    >
+      <div
+        class="md:overflow-none scrollbar-hidden flex h-16 w-full items-center justify-between overflow-x-auto"
+      >
+        <Logo class="mr-6 shrink-0 md:mr-0" />
         <div
-          class="backdrop-filter backdrop-blur-2xl flex items-center justify-between w-full h-16 overflow-x-auto md:overflow-none scrollbar-hidden"
+          class="mr-6 flex shrink-0 items-center justify-between space-x-2 md:mr-0 md:space-x-4"
         >
-          <Logo class="mr-6 md:mr-0 shrink-0" />
-            <div class="flex items-center justify-between shrink-0 space-x-2 md:space-x-4 mr-6 md:mr-0">
           <slot></slot>
         </div>
       </div>
-    </nav>
+    </div>
+  </nav>
 </template>
