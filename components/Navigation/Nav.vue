9208a3134a43aac7e33378507721454417b43628
Karl <karl@kejk.tech>
2022-02-10 07:42:57 +0000

    nav blur

--- components/Navigation/Nav.vue
+++ components/Navigation/Nav.vue
@@ -2,7 +2,7 @@
   <div>
     <nav>
       <div
-        class="w-full fixed z-50 bg-white dark:bg-black mx-auto pl-4 sm:pl-6 lg:px-8"
+        class="w-full fixed z-50 backdrop-blur-2xl bg-white/30 dark:bg-black/30 mx-auto pl-4 sm:pl-6 lg:px-8"
       >
         <div
           class="flex items-center justify-between w-full h-16 overflow-x-auto md:overflow-none scrollbar-hidden"
