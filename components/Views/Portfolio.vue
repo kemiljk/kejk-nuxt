e14cb8ab0d2aef58937dbef6eddc031e7bc5e737
Karl <karl@kejk.tech>
2022-02-17 13:16:56 +0000

    image placement on portfolios

--- components/Views/Portfolio.vue
+++ components/Views/Portfolio.vue
@@ -2,16 +2,16 @@
   <div class="mt-0 pt-4">
     <div class="flex flex-row text-left">
       <div class="col-span-full flex flex-col md:col-span-6">
-        <img
-          :src="portfolio.metadata.featured.imgix_url"
-          class="mb-4 w-full rounded-xl"
-        />
         <Header class="pt-24 text-left md:text-center">
           {{ portfolio.title }}
         </Header>
         <h3 class="text-left md:text-center">
           {{ portfolio.metadata.description }}
         </h3>
+        <img
+          :src="portfolio.metadata.featured.imgix_url"
+          class="mt-4 w-full rounded-xl"
+        />
       </div>
     </div>
     <div>
