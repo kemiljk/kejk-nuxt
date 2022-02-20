5b83136ea1cbedc8b4373d64c4da43c46ad34910
Karl Koch <karl@kejk.tech>
2022-02-20 05:18:41 +0000

    fix: card

--- components/Cards/FeaturesCard.vue
+++ components/Cards/FeaturesCard.vue
@@ -1,32 +1,36 @@
 <template>
-  <a
-    :href="feature.metadata.url"
-    target="`_blank"
-    rel="noreferrer"
-    class="w-full rounded-3xl border border-neutral-200 p-4 text-left transition duration-300 ease-out hover:bg-neutral-50 hover:shadow-lg  dark:border-neutral-800 dark:hover:bg-neutral-900 no-underline cursor-pointer select-none"
+  <div
+    class="w-full cursor-pointer select-none rounded-3xl border border-neutral-200 p-4 text-left transition duration-300 ease-out hover:bg-neutral-50 hover:shadow-lg dark:border-neutral-800 dark:hover:bg-neutral-900"
   >
-        <header class="flex w-full items-center justify-between">
-          <h3
-            class="mb-0 w-full pt-0 text-base font-bold text-black dark:text-white"
-          >
-            {{ feature.title }}
-          </h3>
-          <arrow-up-right-icon
-            class="ml-2 shrink-0 text-neutral-700 dark:text-neutral-300"
-            size="16"
-          ></arrow-up-right-icon>
-        </header>
-        <div class="flex w-full justify-between pt-2">
-          <p
-            class="mb-0 font-mono text-sm text-neutral-700 dark:text-neutral-300"
-          >
-            {{ feature.metadata.published | moment("DD/MM/YYYY") }}
-          </p>
-          <p class="mb-0 font-mono text-sm text-indigo-500">
-            {{ feature.metadata.type }}
-          </p>
-        </div>
-  </a>
+    <a
+      :href="feature.metadata.url"
+      target="`_blank"
+      rel="noreferrer"
+      class="no-underline"
+    >
+      <header class="flex w-full items-center justify-between">
+        <h3
+          class="mb-0 w-full pt-0 text-base font-bold text-black dark:text-white"
+        >
+          {{ feature.title }}
+        </h3>
+        <arrow-up-right-icon
+          class="ml-2 shrink-0 text-neutral-700 dark:text-neutral-300"
+          size="16"
+        ></arrow-up-right-icon>
+      </header>
+      <div class="flex w-full justify-between pt-2">
+        <p
+          class="mb-0 font-mono text-sm text-neutral-700 dark:text-neutral-300"
+        >
+          {{ feature.metadata.published | moment("DD/MM/YYYY") }}
+        </p>
+        <p class="mb-0 font-mono text-sm text-indigo-500">
+          {{ feature.metadata.type }}
+        </p>
+      </div>
+    </a>
+  </div>
 </template>
 
 <script>
