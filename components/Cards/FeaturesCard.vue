976e1ec8de2d6f73c23e001149b3ca47cc75318e
Karl <karl@kejk.tech>
2022-02-20 05:09:36 +0000

    card test

--- components/Cards/FeaturesCard.vue
+++ components/Cards/FeaturesCard.vue
@@ -3,9 +3,9 @@
     :href="feature.metadata.url"
     target="`_blank"
     rel="noreferrer"
-    class="w-full rounded-3xl border border-neutral-200 p-4 text-left transition duration-300 ease-out hover:bg-neutral-50 hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-white dark:border-neutral-800 dark:hover:bg-neutral-900 no-underline cursor-pointer select-none"
+    class="w-full rounded-3xl border border-neutral-200 p-4 text-left transition duration-300 ease-out hover:bg-neutral-50 hover:shadow-lg  dark:border-neutral-800 dark:hover:bg-neutral-900 no-underline cursor-pointer select-none"
   >
-        <header class="flex items-center justify-between">
+        <header class="flex w-full items-center justify-between">
           <h3
             class="mb-0 w-full pt-0 text-base font-bold text-black dark:text-white"
           >
@@ -16,7 +16,7 @@
             size="16"
           ></arrow-up-right-icon>
         </header>
-        <div class="flex justify-between pt-2">
+        <div class="flex w-full justify-between pt-2">
           <p
             class="mb-0 font-mono text-sm text-neutral-700 dark:text-neutral-300"
           >
