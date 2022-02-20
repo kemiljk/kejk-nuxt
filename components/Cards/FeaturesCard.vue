a6a0b734b5549a14902f64a94d8d644ce170c721
Karl <karl@kejk.tech>
2022-02-20 04:41:57 +0000

    select none

--- components/Cards/FeaturesCard.vue
+++ components/Cards/FeaturesCard.vue
@@ -3,10 +3,10 @@
     :href="feature.metadata.url"
     target="`_blank"
     rel="noreferrer"
-    class="mb-0 cursor-pointer"
+    class="mb-0 cursor-pointer select-none"
   >
     <button
-      class="w-full rounded-3xl border border-neutral-200 p-4 text-left transition duration-300 ease-out hover:bg-neutral-50 hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-white dark:border-neutral-800 dark:hover:bg-neutral-900"
+      class="w-full rounded-3xl border border-neutral-200 p-4 text-left transition duration-300 ease-out hover:bg-neutral-50 hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-white dark:border-neutral-800 dark:hover:bg-neutral-900 select-none"
     >
       <div>
         <header class="flex items-center justify-between">
