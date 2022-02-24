c92d1277afaee4cf930119648c989c87f4ecdc04
Karl <karl@kejk.tech>
2022-02-24 22:31:39 +0000

    font size

--- components/Atoms/Header.vue
+++ components/Atoms/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="container mx-auto max-w-xl">
-    <h1 class="pb-6 text-5xl font-bold leading-[1.125em]">
+    <h1 class="pb-6 text-6xl font-bold leading-[1.125em]">
       <slot />
     </h1>
   </header>
