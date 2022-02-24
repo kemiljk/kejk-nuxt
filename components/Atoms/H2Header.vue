604df5809cc18daadff9b27270215b8357671c6e
Karl <karl@kejk.tech>
2022-02-24 22:40:10 +0000

    snug leading h2

--- components/Atoms/H2Header.vue
+++ components/Atoms/H2Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="w-full">
-    <h2 class="text-3xl text-black dark:text-white">
+    <h2 class="text-3xl text-black dark:text-white leading-[1.125em]">
       <slot />
     </h2>
   </header>
