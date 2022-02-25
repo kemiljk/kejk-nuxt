7c3994dfab08946c3b9c476c8d89d23906a2186c
Karl <karl@kejk.tech>
2022-02-25 20:35:39 +0000

    leading

--- components/Atoms/Header.vue
+++ components/Atoms/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="container mx-auto max-w-xl">
-    <h1 class="pb-6 text-6xl font-bold leading-[0.8em]">
+    <h1 class="pb-6 text-6xl font-bold leading-[0.85em]">
       <slot />
     </h1>
   </header>
