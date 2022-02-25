6d66dffbe1d15c5dd02822baac2a898e97c8835f
Karl <karl@kejk.tech>
2022-02-25 19:37:24 +0000

    leading

--- components/Atoms/H2Header.vue
+++ components/Atoms/H2Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="w-full">
-    <h2 class="text-3xl text-black dark:text-white leading-[1.125em]">
+    <h2 class="text-3xl text-black dark:text-white leading-[1em]">
       <slot />
     </h2>
   </header>
