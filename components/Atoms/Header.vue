f142801e34abdf78c0b7084e59c915ddc701bb70
Karl <karl@kejk.tech>
2022-02-26 08:17:53 +0000

    fix: title size

--- components/Atoms/Header.vue
+++ components/Atoms/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="container mx-auto max-w-xl">
-    <h1 class="pb-6 text-6xl font-bold leading-[1em]">
+    <h1 class="pb-6 text-5xl md:text-6xl font-bold leading-[1em]">
       <slot />
     </h1>
   </header>
